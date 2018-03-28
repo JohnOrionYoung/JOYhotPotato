11470a0c104bcaa34125b70b855438ada6bb4c02
johnorionyoung <hi@johnorionyoung.com>
2018-03-27 19:40:42 -0700

    backend files addes

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+// set port 
+var port = process.env.PORT || 8080
+
+app.use(express.static(__dirname + "/src"));
+
+// routes
+
+app.get("/", function(req, res) {
+    res.render("index")
+})
+
+app.listen(port, function() {
+    console.log("app running");
+})
