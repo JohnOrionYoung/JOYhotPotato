430a8a2f8c29b5fcddd833b5e102febe133df5bd
johnorionyoung <hi@johnorionyoung.com>
2018-08-22 09:48:06 -0700

    rollback

--- src/js/JoyArt.js
+++ src/js/JoyArt.js
@@ -175,17 +175,3 @@ function queryParams() {
   } else {
       artRow = $('#art-row');
   }
-
-var express = require('express');
-var http = require('http');
-var enforce = require('express-sslify');
- 
-var app = express();
- 
-// Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
-// a load balancer (e.g. Heroku). See further comments below
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
- 
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
