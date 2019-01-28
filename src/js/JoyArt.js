4af79b426a0eabe3062ee20b34db1ac9d408265e
johnorionyoung <hi@johnorionyoung.com>
2019-01-28 10:08:15 -0800

    style matching

--- src/js/JoyArt.js
+++ src/js/JoyArt.js
@@ -169,38 +169,3 @@ jQuery(document).ready(
     App.init();
   }
 );
-
-
-//Video Button
-
-var videoPlayButton,
-	videoWrapper = document.getElementsByClassName('video-wrapper')[0],
-    video = document.getElementsByTagName('video')[0],
-    videoMethods = {
-        renderVideoPlayButton: function() {
-            if (videoWrapper.contains(video)) {
-				this.formatVideoPlayButton()
-                video.classList.add('has-media-controls-hidden')
-                videoPlayButton = document.getElementsByClassName('video-overlay-play-button')[0]
-                videoPlayButton.addEventListener('click', this.hideVideoPlayButton)
-            }
-        },
-
-        formatVideoPlayButton: function() {
-            videoWrapper.insertAdjacentHTML('beforeend', '\
-                <svg class="video-overlay-play-button" viewBox="0 0 200 200" alt="Play video">\
-                    <circle cx="100" cy="100" r="90" fill="none" stroke-width="16" stroke="#FFE70D"/>\
-                    <polygon points="70, 55 70, 145 145, 100" fill="#FFE70D"/>\
-                </svg>\
-            ')
-        },
-
-        hideVideoPlayButton: function() {
-            video.play()
-            videoPlayButton.classList.add('is-hidden')
-            video.classList.remove('has-media-controls-hidden')
-            video.setAttribute('controls', 'controls')
-        }
-	}
-
-videoMethods.renderVideoPlayButton()
